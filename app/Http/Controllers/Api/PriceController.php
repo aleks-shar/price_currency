fb327151290a2da20f517d796d64f36575e3102e
aleks-shar <alexsharshakov@gmail.com>
2025-07-22 23:11:26 +0500

    Add PriceRequest

--- app/Http/Controllers/Api/PriceController.php
+++ app/Http/Controllers/Api/PriceController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PriceRequest;
 use App\Http\Resources\ProductResource;
 use App\Services\PriceService;
 use Illuminate\Http\Resources\Json\ResourceCollection;
@@ -12,10 +13,11 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 final class PriceController extends Controller
 {
     /**
+     * @param PriceRequest $request
      * @param PriceService $service
      * @return ResourceCollection
      */
-    public function index(PriceService $service): ResourceCollection
+    public function index(PriceRequest $request, PriceService $service): ResourceCollection
     {
         return ProductResource::collection($service->getAllPrices());
     }
---
+++ app/Http/Requests/PriceRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+final class PriceRequest extends FormRequest
+{
+    /**
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'currency' => 'nullable|in:RUB,USD,EUR',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'currency.in' => 'Currency must be one of: RUB, USD, EUR',
+        ];
+    }
+}
